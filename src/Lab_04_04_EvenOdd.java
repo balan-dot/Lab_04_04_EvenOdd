e6d2dd8888b02713b88aad1960b0ea02f78d4d76
ryanb <balan@mail.uc.edu>
2026-09-21 13:34:06 -0400

    Even Odd program

--- src/Lab_04_04_EvenOdd.java
+++ src/Lab_04_04_EvenOdd.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Lab_04_04_EvenOdd {
+    static void main() {
+
+        int numToExamine = 2;
+
+        if (numToExamine % 2 == 0) {
+            IO.println("The number " + numToExamine + " is even");
+        } else {
+            IO.println("The number " + numToExamine + " is odd");
+        }
+    }
 }
+
+
